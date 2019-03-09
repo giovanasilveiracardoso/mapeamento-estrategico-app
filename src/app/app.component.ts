21ae7f5b24d310ffa4df13f8f47b8d6a655d44fe
Giovana <giovanasilveiracardoso@gmail.com>
2019-03-09 17:42:29 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mea-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mapeamento-estrategico-app';
+}
